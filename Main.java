d17cfa0becae95a594e26143763ce906412ce064
TANZEEL MIRZA <33144278+tmtanzeel@users.noreply.github.com>
2019-03-10 23:47:01 +0530

    updated monitor method

--- Main.java
+++ Main.java
@@ -46,29 +46,30 @@ public class Main extends Application {
 	  new Thread(task1).start();
   }
   
-  public void monitor() {	  
-	  task1=new Task<Void>() {
-		  @Override
-		  protected Void call() {
-			  System.out.println("run called");
-			  int i=1;
-			  while(true) {
-				  System.out.println("inside while");
-				  try {
-					  Thread.sleep(1000);
-					  task1.messageProperty().addListener((observable, oldValue, newValue) -> {
-						  Image image = new Image(getClass().getResourceAsStream("images/"+newValue+".png"));
-						  imv.setImage(image);
-						  label1.setGraphic(imv);
-						});
-				  }
-				  catch(Exception e) {
-					  
-				  }				  
-			  }
-		 }
-	  };
-  } 
+  public void monitor() {
+	    task1 = new Task<Void>() {
+	        @Override
+	        protected Void call() {
+	            System.out.println("run called");
+	            int i = 1;
+	            while (true) {
+	                try {
+	                    Thread.sleep(1000);
+	                    updateMessage(i + ".png");
+	                    System.out.println("i: " + i);
+	                } catch (Exception e) {
+
+	                }
+	                i++;
+	            }
+	        }
+	    };
+	    task1.messageProperty().addListener((observable, oldValue, newValue) -> {
+	        System.out.println(newValue);
+	        Image image = new Image(getClass().getResourceAsStream("images/" + newValue));
+	        imv.setImage(image);
+	    });
+	}
  
   
   public static void main(String[] args) {
